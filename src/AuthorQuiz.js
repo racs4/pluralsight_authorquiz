efbb2cd78aa561831143f5b54bf285d0b4e7a1a3
Rheidner <rheidner.achiles@gmail.com>
2020-05-25 14:08:10 -0300

    feat: adiciona header e footer

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -4,8 +4,9 @@ import logo from './logo.svg';
 
 function Hero() {
   return (
-    <div>
-      Teste
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p> Guess the correct book from the author of the photo</p>
     </div>
   );
 }
@@ -19,12 +20,16 @@ function Continue() {
 }
 
 function Footer() {
-  return (<div />);
+  return (
+    <div className="col-10 offset-1 row">
+      <p>All images are from <a href="#">Wikimedia Commons</a> and are in public domain.</p>
+    </div>
+  );
 }
 
 function AuthorQuiz() {
   return (
-    <div>
+    <div className="container-fluid">
       <Hero />
       <Turn />
       <Continue />
