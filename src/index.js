a7adfd06799e12731efd8666b967a19b6e5db987
Rheidner <rheidner.achiles@gmail.com>
2020-05-25 14:24:12 -0300

    data: adiciona dados necessario a aplicacao

--- src/index.js
+++ src/index.js
@@ -4,6 +4,48 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.png',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerers Stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: ['The Shining', 'IT']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/williamshakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  }
+];
+
+
 ReactDOM.render(
   < AuthorQuiz />,
   document.getElementById('root')
