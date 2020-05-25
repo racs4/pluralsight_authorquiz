e810de9c72938e314ffc290c3d9ed28b6c0ace56
Rheidner <rheidner.achiles@gmail.com>
2020-05-25 13:59:26 -0300

    init: cria base para os modulos

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 
-function AuthorQuiz() {
+
+function Hero() {
   return (
     <div>
       Teste
+    </div>
+  );
+}
+
+function Turn() {
+  return (<div />);
+}
+
+function Continue() {
+  return (<div />);
+}
+
+function Footer() {
+  return (<div />);
+}
+
+function AuthorQuiz() {
+  return (
+    <div>
+      <Hero />
+      <Turn />
+      <Continue />
+      <Footer />
     </div>
   );
 }
